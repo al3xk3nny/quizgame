78ce70ad2c77398e5f02ba17d18afa352b80e7bd
Alex Kenny <alexkenny1@gmail.com>
2018-10-11 14:01:20 +0000

    Implemented Exit code

--- quiz.py
+++ quiz.py
@@ -1,9 +1,18 @@
-print("Quiz Game")
-print("----------")
-print()
-print("1. Run quiz")
-print("2. Enter a question")
-print("3. Exit")
-print()
+def show_menu()
+    print("Quiz Game")
+    print("----------")
+    print()
+    print("1. Run quiz")
+    print("2. Enter a question")
+    print("3. Exit")
+    print()
+    
+    option = input("Enter option: ")
+    return option
 
-option = input("Enter option: ")
+while True:
+    option = show_menu() # No conflict with naming variable "option" as "option" variable in show_menu function appears within a function.
+    
+    if option == "3": # Important to use "" as comparing against a string in show_menu function above.
+        break
+
