df7f5b2f6f7d04434406f3059e406d7f4db35060
Alex Kenny <alexkenny1@gmail.com>
2018-10-11 14:58:10 +0000

    Question write functional

--- quiz.py
+++ quiz.py
@@ -1,4 +1,4 @@
-def show_menu()
+def show_menu():
     print("Quiz Game")
     print("----------")
     print()
@@ -10,8 +10,22 @@ def show_menu()
     option = input("Enter option: ")
     return option
 
+def add_a_question():
+    question = input("Enter a question: ")
+    answer = input("Ok then tell me, " + question + ": ")
+
+    with open("questions.txt", "a") as f: 
+        line= question + "|" + answer + "\n"
+        f.write(line)
+
 while True:
     option = show_menu() # No conflict with naming variable "option" as "option" variable in show_menu function appears within a function.
+    
+    if option == "1":
+        print("You picked run a quiz")
+        
+    if option == "2":
+        add_a_question()
     
     if option == "3": # Important to use "" as comparing against a string in show_menu function above.
         break
