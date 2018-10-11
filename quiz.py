8a6ff761d8b4852b187b17efb1f74e3c95f9c43c
Alex Kenny <alexkenny1@gmail.com>
2018-10-11 16:01:25 +0000

    Added run quiz function

--- quiz.py
+++ quiz.py
@@ -10,6 +10,28 @@ def show_menu():
     option = input("Enter option: ")
     return option
 
+
+def ask_questions():
+    with open("questions.txt") as f:
+        questions = f.read().split("\n")[:-1] # Read the text file using file handel, f. This returns a string. Split the string at "\n". When you split a string you return a list. The [:-1] returns everything bar the last entry. This is important as in the add_a_question function we added a "\n" to ensure each question|answer combination returned on a separate line giving us an empty new line at the end. 
+        
+    score = 0
+        
+    for q in questions:
+        # question = q.split("|")[0]
+        # answer = q.split("|")[1]
+        
+        # Also written as this;
+        question, answer = q.split("|")
+        guess = input(question)
+        print(guess)
+        
+        if guess == answer:
+            score += 1
+            
+    print("You scored {0}".format(score))
+    
+
 def add_a_question():
     question = input("Enter a question: ")
     answer = input("Ok then tell me, " + question + ": ")
@@ -18,11 +40,12 @@ def add_a_question():
         line= question + "|" + answer + "\n"
         f.write(line)
 
+
 while True:
     option = show_menu() # No conflict with naming variable "option" as "option" variable in show_menu function appears within a function.
     
     if option == "1":
-        print("You picked run a quiz")
+        ask_questions()
         
     if option == "2":
         add_a_question()
