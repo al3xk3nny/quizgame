1626d8aa157be28d3a953c1632cfc9b8dd270a15
Alex Kenny <alexkenny1@gmail.com>
2018-10-12 10:18:00 +0000

    Minor update to tryargs file  before push

--- tryargs.py
+++ tryargs.py
@@ -1,3 +1,5 @@
+# This is a dummy file to try sys.argv. It is not connected to "quiz.py". You still need to import sys into the "quiz.py" file.
+
 import sys
 print("This is the name of the script: ", sys.argv[0])
 print("Number of arguments: ", len(sys.argv))
