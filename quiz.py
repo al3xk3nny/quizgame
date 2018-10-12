7ccabe39f1625b79cf6520ed73afe10fb821653f
Alex Kenny <alexkenny1@gmail.com>
2018-10-12 09:07:50 +0000

    Guess, answer case insensitive

--- quiz.py
+++ quiz.py
@@ -10,12 +10,13 @@ def show_menu():
     option = input("Enter option: ")
     return option
 
-
+# 1. Run quiz.
 def ask_questions():
     with open("questions.txt") as f:
         questions = f.read().split("\n")[:-1] # Read the text file using file handel, f. This returns a string. Split the string at "\n". When you split a string you return a list. The [:-1] returns everything bar the last entry. This is important as in the add_a_question function we added a "\n" to ensure each question|answer combination returned on a separate line giving us an empty new line at the end. 
         
     score = 0
+    total = len(questions)
         
     for q in questions:
         # question = q.split("|")[0]
@@ -23,15 +24,18 @@ def ask_questions():
         
         # Also written as this;
         question, answer = q.split("|")
+        
         guess = input(question)
         print(guess)
         
-        if guess == answer:
+        if guess.lower() == answer.lower(): # Case insensitive at point of comparison.
             score += 1
             
-    print("You scored {0}".format(score))
+    # print("You scored {0}".format(score) + " out of {0}".format(len(questions)))
+    # Can also be written like this if you add a variable called "total" above and call it below.
+    print("You scored {0} out of {1}".format(score, total))
     
-
+# 2. Enter a question.
 def add_a_question():
     question = input("Enter a question: ")
     answer = input("Ok then tell me, " + question + ": ")
@@ -40,7 +44,7 @@ def add_a_question():
         line= question + "|" + answer + "\n"
         f.write(line)
 
-
+# 3. Exit.
 while True:
     option = show_menu() # No conflict with naming variable "option" as "option" variable in show_menu function appears within a function.
     
